da7405dde91aa5df2a3e1b82e2e9bbe13ec14cad
Giulio <giulio@debian.debian>
2017-02-25 15:19:27 +0100

    Functional test now work with 2 webdrivers.

--- movieapp_backend/functional_tests/tests.py
+++ movieapp_backend/functional_tests/tests.py
@@ -7,11 +7,13 @@ import sys
 import time
 
 sys.path.append(os.getcwd())
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movieapp_backend.settings")  # or whatever
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "movieapp_backend.settings")  # or whatever
 django.setup()
 
 
 class NewVisitorTest(LiveServerTestCase):
+
     @classmethod
     def setUpClass(cls):
 
@@ -30,18 +32,30 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.set_window_size(768, 768)
+        self.browser.set_window_position(0,0)
         self.browser.implicitly_wait(3)
-
+        self.browser_2 = webdriver.Firefox()
+        self.browser_2.set_window_size(768, 768)
+        time.sleep(0.5)
+        self.browser_2.set_window_position(683, 0)
+        self.browser_2.implicitly_wait(3)
+        
     def tearDown(self):
         self.browser.quit()
+        self.browser_2.quit()        
 
     def test_home_page_when_logged(self):
 
+        # Opening Signup page and registering new user.
+
         self.browser.get('%s%s' % (self.server_url, '/signup'))
+        self.browser_2.get('%s%s' % (self.server_url, '/signup'))        
         self.assertIn('Signup', self.browser.title)
         username = self.browser.find_element_by_id('username_input')
         password = self.browser.find_element_by_id('password_input')
-        verify_password = self.browser.find_element_by_id('verify_password_input')
+        verify_password = self.browser.find_element_by_id(
+            'verify_password_input')
         email = self.browser.find_element_by_id('email_input')
         username.send_keys('bbbb')
         password.send_keys('bbbb')
@@ -50,43 +64,50 @@ class NewVisitorTest(LiveServerTestCase):
         button = self.browser.find_element_by_id('signup_button')
         button.click()
         time.sleep(2)
+        #Redirected to HomePage then logout and register friend user
         self.assertIn('Homepage', self.browser.title)
         self.browser.get('/logout/')
         time.sleep(0.5)
         self.assertIn('Login', self.browser.title)
-        self.browser.find_element_by_id('sign_in_url').click()
-        self.browser.implicitly_wait(3)
-        username = self.browser.find_element_by_id('username_input')
-        password = self.browser.find_element_by_id('password_input')
-        verify_password = self.browser.find_element_by_id('verify_password_input')
-        email = self.browser.find_element_by_id('email_input')
+        # self.browser_2.find_element_by_id('sign_in_url').click()
+        # self.browser_2.implicitly_wait(3)
+        username = self.browser_2.find_element_by_id('username_input')
+        password = self.browser_2.find_element_by_id('password_input')
+        verify_password = self.browser_2.find_element_by_id(
+            'verify_password_input')
+        email = self.browser_2.find_element_by_id('email_input')
         username.send_keys('aaaa')
         password.send_keys('asdasdasd')
         verify_password.send_keys('asdasdasd')
         email.send_keys('aaaa@mail.com')
-        button = self.browser.find_element_by_id('signup_button')
+        button = self.browser_2.find_element_by_id('signup_button')
         button.click()
         time.sleep(2)
-
-        self.browser.find_element_by_id('username')
-        self.browser.find_element_by_id('dropdown_logout_url')
-        friend_search = self.browser.find_element_by_id('search_friends_bar')
+        #Search for friend profile
+        self.browser_2.find_element_by_id('username')
+        self.browser_2.find_element_by_id('dropdown_logout_url')
+        friend_search = self.browser_2.find_element_by_id('search_friends_bar')
         friend_search.send_keys('bbbb')
-        search_friend_button = self.browser.find_element_by_id('search_friends_button')
+        search_friend_button = self.browser_2.find_element_by_id(
+            'search_friends_button')
         search_friend_button.click()
         time.sleep(1)
-        self.assertIn('Search Results', self.browser.title)
-        result = self.browser.find_element_by_link_text('bbbb')
+        self.assertIn('Search Results', self.browser_2.title)
+        result = self.browser_2.find_element_by_link_text('bbbb')
         result.click()
         time.sleep(1)
-        self.assertIn('Profile of bbbb', self.browser.title)
-        add_friend_button = self.browser.find_element_by_id('add_friend_button')
+        # Check friend profile and send him friend request.
+        self.assertIn('Profile of bbbb', self.browser_2.title)
+        add_friend_button = self.browser_2.find_element_by_id(
+            'add_friend_button')
         add_friend_button.click()
         time.sleep(1)
-        self.assertIn('Friend Request Sent', self.browser.find_element_by_class_name("main").get_attribute('innerHTML'))
-        self.browser.get('/logout/')
+        self.assertIn('Friend Request Sent', self.browser_2.find_element_by_class_name(
+            "main").get_attribute('innerHTML'))
+        # self.browser.get('/logout/')
         time.sleep(0.5)
-        self.assertIn('Login', self.browser.title)
+        # Login as and check settings page 
+        # self.assertIn('Login', self.browser.title)
         username = self.browser.find_element_by_id('username_input')
         password = self.browser.find_element_by_id('password_input')
         username.send_keys('bbbb')
@@ -96,17 +117,19 @@ class NewVisitorTest(LiveServerTestCase):
         login = self.browser.find_element_by_id('login_button')
         login.click()
         time.sleep(1)
-
+        #Accept friend request
         self.browser.get('/settings/')
         self.browser.find_element_by_id("friend_requests")
         self.browser.find_element_by_class_name("friend_request_message")
-        accept_button = self.browser.find_element_by_xpath("//button[contains(.,'Accept')]")
+        accept_button = self.browser.find_element_by_xpath(
+            "//button[contains(.,'Accept')]")
         accept_button.click()
         time.sleep(1)
         friend_list = self.browser.find_element_by_id('friends_list')
         self.assertIn('aaaa', friend_list.get_attribute('innerHTML'))
 
         self.browser.get(self.server_url)
+        # Send Post to friend
         send_post = self.browser.find_element_by_id('send_post')
         send_post.click()
         time.sleep(10)
@@ -114,7 +137,6 @@ class NewVisitorTest(LiveServerTestCase):
         title = self.browser.find_element_by_id('title_input')
         img = self.browser.find_element_by_id('img_input')
         url = self.browser.find_element_by_id('url_input')
-        # rating = self.browser.find_element_by_id('rating_container')
         comment = self.browser.find_element_by_id('comment_textarea')
         self.browser.find_element_by_id('send_to_button')
         send_post = self.browser.find_element_by_id('send_post_button')
@@ -124,10 +146,10 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.find_element_by_class_name('glyphicon').click()
         time.sleep(5)
         comment.send_keys('One of the best fighting movies I have ever seen.')
-        # send_to.click()
         send_post.click()
         time.sleep(2)
         # Check Settings page. No redirects because I'm still logged in.
+        # Change name, avatar, password and friendlist.
         settings = self.browser.find_element_by_id('dropdown_settings_link')
         settings.click()
         time.sleep(2)
@@ -136,15 +158,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.find_element_by_id('change_name_form')
         change_name = self.browser.find_element_by_id('change_name_input')
         change_name.send_keys('My new cool name')
-        submit_change = self.browser.find_element_by_id('submit_change_name_button')
+        submit_change = self.browser.find_element_by_id(
+            'submit_change_name_button')
         submit_change.click()
         time.sleep(1)
         self.browser.find_element_by_id('change_password_button').click()
-        change_password = self.browser.find_element_by_id('change_password_input')
+        change_password = self.browser.find_element_by_id(
+            'change_password_input')
         change_password.send_keys('mynewcoolpassword')
-        verify_change_password = self.browser.find_element_by_id('verify_password_input')
+        verify_change_password = self.browser.find_element_by_id(
+            'verify_password_input')
         verify_change_password.send_keys('mynewcoolpassword')
-        submit_change = self.browser.find_element_by_id('submit_change_password_button')
+        submit_change = self.browser.find_element_by_id(
+            'submit_change_password_button')
         submit_change.click()
         time.sleep(1)
         self.browser.find_element_by_id('name_tag')
@@ -154,24 +180,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.find_element_by_id('avatar')
         self.browser.find_element_by_id('friends_list')
 
-        self.browser.get('/login')
+        # self.browser.get('/login')
 
         time.sleep(2)
+        # Check if post was received then delete account
+        # self.assertIn('Login', self.browser.title)
+        # self.browser.find_element_by_id('username_input').send_keys('aaaa')
+        # self.browser.find_element_by_id(
+        #     'password_input').send_keys('asdasdasd')
 
-        self.assertIn('Login', self.browser.title)
-        self.browser.find_element_by_id('username_input').send_keys('aaaa')
-        self.browser.find_element_by_id('password_input').send_keys('asdasdasd')
-
-        self.browser.find_element_by_id('login_button').click()
-        time.sleep(4)
-        self.assertIn('Ip Man', self.browser.find_element_by_class_name("main").get_attribute("innerHTML"))
-        self.browser.get('/settings')
-        self.browser.find_element_by_id('show_delete_account_modal').click()
-        time.sleep(3)
-        self.browser.switch_to.alert
-        self.browser.find_element_by_id('delete_account_button').click()
+        # self.browser.find_element_by_id('login_button').click()
+        self.browser_2.get(self.server_url)
         time.sleep(1)
-        self.assertIn('Login', self.browser.title)
+        self.assertIn('Ip Man', self.browser_2.find_element_by_class_name(
+            "main").get_attribute("innerHTML"))
+        self.browser_2.get('/settings')
+        self.browser_2.find_element_by_id('show_delete_account_modal').click()
+        time.sleep(3)
+        self.browser_2.switch_to.alert
+        self.browser_2.find_element_by_id('delete_account_button').click()
+        time.sleep(1)
+        self.assertIn('Login', self.browser_2.title)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
